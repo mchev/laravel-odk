1fabfc08710c3b8bb34fd216676c3a558a152116
mchev <martin.chevignard@gmail.com>
2021-07-24 13:08:31 +0200

    Users tests

--- tests/Unit/UsersTest.php
+++ tests/Unit/UsersTest.php
@@ -2,16 +2,39 @@
 
 namespace Mchev\LaravelOdk\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Mchev\LaravelOdk\Tests\TestCase;
 use OdkCentral;
 
 class UsersTest extends TestCase
 {
-
-    public function test_can_get_users(): void
+    
+    /**
+     * Test if can get users list.
+     *
+     * @return void
+     */
+    public function test_can_get_users()
     {
-        //$users = OdkCentral::users()->get();
+        $users = OdkCentral::users()->get();
 
-        $this->assertEquals(1, 1);
+        $this->assertNotNull($users);
     }
+
+    /**
+     * Test if can get a user by id.
+     *
+     * @return void
+     */
+    public function test_can_get_user()
+    {
+        $userId = 115;
+
+        $user = OdkCentral::users(115)->get();
+
+        $this->assertEquals($user->id, $userId);
+    }
+
 
 }
